a57a6f372b77a0ad72b37321938a5ce4daaccf6a
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-06-07 08:12:18 +0530

    database is connected

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { nanoid } from "nanoid";
-
+import  connetDB from "./config/connectDb.js"
+connetDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
